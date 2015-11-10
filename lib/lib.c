cb5e04af8446ffe4430883e0670e3ffcb02466e7
Nikolaos Korasidis <renelvon@gmail.com>
2015-11-11 00:52:59 +0200

    Lib: Prepend llama prefix to avoid name clashes with standard C library

--- lib/lib.c
+++ lib/lib.c
@@ -14,40 +14,40 @@ unit check_err(int ret, const char *s) {
 }
 
 // PRINTING TO STDOUT
-unit print_int(int n){
+unit llama_print_int(int n){
     int ret = printf("%d", n);
     check_err(ret, __func__);
 }
 
-unit print_bool(bool b){
+unit llama_print_bool(bool b){
     int ret = b ? printf("true") : printf("false");
     check_err(ret, __func__);
 }
 
-unit print_char(char c){
+unit llama_print_char(char c){
     int ret = putchar(c);
     check_err(ret, __func__);
 }
 
-unit print_float(double d){
+unit llama_print_float(double d){
     int ret = printf("%lf", d);
     check_err(ret, __func__);
 }
 
-unit print_string(const char* s){
+unit llama_print_string(const char* s){
     int ret = printf("%s", s);
     check_err(ret, __func__);
 }
 
 // READING FROM STDIN
-int read_int(){
+int llama_read_int(){
     int n, ret;
     ret = scanf("%d", &n);
     check_err(ret, __func__);
     return n;
 }
 
-bool read_bool(){
+bool llama_read_bool(){
     // TODO: This should probably read a string, not an int.
     int n;
     bool b;
@@ -56,20 +56,20 @@ bool read_bool(){
     return b;
 }
 
-char read_char(){
+char llama_read_char(){
     int ret = getchar();
     check_err(ret, __func__);
     return (char)ret;
 }
 
-double read_float(){
+double llama_read_float(){
     double d;
     int ret = scanf("%lf", &d);
     check_err(ret, __func__);
     return d;
 }
 
-unit read_string(char* s, int n){
+unit llama_read_string(char* s, int n){
     char temp;
     
     for(int i = 0; i < n; ++i){
@@ -80,71 +80,71 @@ unit read_string(char* s, int n){
 }
 
 // BASIC MATH
-int abs(int n){
+int llama_abs(int n){
     return abs(n);
 }
 
-double fabs(double n){
+double llama_fabs(double n){
     return fabs(n);
 }
 
-double sqrt(double n){
+double llama_sqrt(double n){
     return sqrt(n);
 }
 
-double sin(double n){
+double llama_sin(double n){
     return sin(n);
 }
 
-double cos(double n){
+double llama_cos(double n){
     return cos(n);
 }
 
-double tan(double n){
+double llama_tan(double n){
     return tan(n);
 }
 
-double atan(double n){
+double llama_atan(double n){
     return atan(n);
 }
 
-double exp(double n){
+double llama_exp(double n){
     return exp(n);
 }
 
-double ln(double n){
+double llama_ln(double n){
     return log(n);
 }
 
-double pi(){
+double llama_pi(){
     return M_PI;
 }
 
-unit incr(int *n){
+unit llama_incr(int *n){
     (*n)++;
 }
 
-unit decr(int *n){
+unit llama_decr(int *n){
     (*n)--;
 }
 
-double float_of_int(int n){
+double llama_float_of_int(int n){
     return (double)n;
 }
 
-int int_of_float(double d){
+int llama_int_of_float(double d){
     return (double)d;
 }
 
-int round(double n){
+int llama_round(double n){
     return (int)round(n);
 }
 
-int int_of_char(char c){
+int llama_int_of_char(char c){
     return (int)c;
 }
 
-char char_of_int(int n){
+char llama_char_of_int(int n){
     return (char)n;
 }
 
